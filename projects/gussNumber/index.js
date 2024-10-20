9de7a6962590071c4353d4deea9a041ef1bd4cae
James Hujiang <james.hujiang@gmail.com>
2024-10-20 14:24:44 +0100

    guess

--- projects/gussNumber/index.js
+++ projects/gussNumber/index.js
@@ -1,9 +1,32 @@
-let ifCorrect = false;
+let running = true;
 let attempts = 0;
+let minNum = 1;
+let maxNum = 100;
+let myNumber = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum
+let guess
 
-//while(!ifCorrect){
-    window.prompt('Input a number between 1-100');
-    attempts ++;
+while(running){
+    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+    guess = Number(guess);
+    if(isNaN(guess)){
+        window.alert('Please enter a valid number!');
+    }
+    else {
+        if(guess > maxNum || guess < minNum){
+            window.alert('Please enter a valid number!')
+        }
+        else {
+            attempts ++;
+            if (guess > myNumber){
+                window.alert('Too big!');
+            }
+            else if (guess < myNumber){
+                window.alert('Too small!')
+            }
+            else {
+                window.alert(`Correct! The number is ${myNumber}, tried ${attempts} times.`);
+                running = false ;
+            }
+        }
+    }
 }
-
-
