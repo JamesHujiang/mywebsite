812664a7fedb777d7671b5d85a307873d1c5232e
James Hujiang <james.hujiang@gmail.com>
2024-10-21 16:09:47 +0100

    change

--- projects/toss/index.js
+++ projects/toss/index.js
@@ -1,7 +1,7 @@
 document.getElementById('myBut').onclick = function(){
     Math.round(Math.random()) ?
-     document.getElementById('myLabel').textContent = '☺' :
-     document.getElementById('myLabel').textContent = '☹';
+     document.getElementById('myLabel').textContent = '∞' :
+     document.getElementById('myLabel').textContent = '☯';
 }
 
 
