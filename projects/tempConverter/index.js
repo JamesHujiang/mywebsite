2ed5f6a8ff92450aa0cfec002b5f9ae3f5f54708
James Hujiang <james.hujiang@gmail.com>
2024-10-24 20:43:31 +0100

    change

--- projects/tempConverter/index.js
+++ projects/tempConverter/index.js
@@ -1,19 +1,21 @@
-/*
-let myNum = document.getElementById('myNum').value;
-// const tempType = document.getElementById('tempType');
-const convBt = document.getElementById('convBt');
+const numBox = document.getElementById('numBox');
+const tempType = document.getElementById('tempType');
+const cToF = document.getElementById('cToF');
+const fToC = document.getElementById('fToC');
+const result = document.getElementById('result');
 
-convBt.onclick() = function(){
-    myNum = Number(myNum);
-    if (myNum == NaN){
-        window.alert('Please input valid number!');
+let temp
+function converter(){
+    temp = Number(numBox.value);
+    if (cToF.checked){
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + '°F';
+    }
+    else if (fToC.checked){
+        temp = (temp - 32) * 5 / 9;
+        result.textContent = temp.toFixed(1) + '°C';
     }
     else {
-        window.alert(`myNum`);
+        result.textContent = 'Please select a unit!';
     }
 }
-
-*/
-
-let myNum = document.getElementById('numBox');
-document.getElementById('result').textContent = myNum.value;
