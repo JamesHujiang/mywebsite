852d6058eda97b48f9b00e29e0805468ef73efdc
James Hujiang <james.hujiang@gmail.com>
2024-10-25 06:02:59 +0100

    change

---
+++ projects/diceRoller/index.js
@@ -0,0 +1,21 @@
+const numBox = document.getElementById('numBox');
+const tempType = document.getElementById('tempType');
+const cToF = document.getElementById('cToF');
+const fToC = document.getElementById('fToC');
+const result = document.getElementById('result');
+
+let temp
+function converter(){
+    temp = Number(numBox.value);
+    if (cToF.checked){
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + '°F';
+    }
+    else if (fToC.checked){
+        temp = (temp - 32) * 5 / 9;
+        result.textContent = temp.toFixed(1) + '°C';
+    }
+    else {
+        result.textContent = 'Please select a unit!';
+    }
+}
