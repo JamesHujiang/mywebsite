6ebe916341f8fe350ce3c364b67cf0f5977cfd11
James Hujiang <james.hujiang@gmail.com>
2024-11-24 07:17:24 +0000

    change

---
+++ projects/slidePic/index.js
@@ -0,0 +1,41 @@
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalID = null;
+
+
+
+document.addEventListener("DOMContentLoaded", initrializeSlider);
+
+function initrializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalID = setInterval(nextSlide, 5000);
+    }
+}
+
+function showSlide(index){
+    if(slideIndex >= slides.length){
+        slideIndex = 0;
+    }
+    else if(slideIndex < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    })
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+    clearInterval(intervalID)
+    slideIndex --;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    slideIndex ++;
+    showSlide(slideIndex);
+}
+
+
